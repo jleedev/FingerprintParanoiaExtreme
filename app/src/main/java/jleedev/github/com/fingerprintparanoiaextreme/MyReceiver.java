4b0dcfaf5cd7d3f35817a20b410192b673a98a48
Josh Lee <jleedev@gmail.com>
2016-05-04 19:44:54 -0400

    initial commit

---
+++ app/src/main/java/jleedev/github/com/fingerprintparanoiaextreme/MyReceiver.java
@@ -0,0 +1,36 @@
+package jleedev.github.com.fingerprintparanoiaextreme;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends DeviceAdminReceiver {
+    private final String TAG = "MyReceiver";
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+    }
+
+    @Override
+    public void onPasswordFailed(Context context, Intent intent) {
+        ComponentName name = new ComponentName(context, MyReceiver.class);
+        DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        int attempts = dpm.getCurrentFailedPasswordAttempts();
+        dpm.setKeyguardDisabledFeatures(name, DevicePolicyManager.KEYGUARD_DISABLE_FINGERPRINT);
+        Log.d(TAG, String.format("Disabling fingerprint after %d failed unlock attempts", attempts));
+    }
+
+    @Override
+    public void onPasswordSucceeded(Context context, Intent intent) {
+        ComponentName name = new ComponentName(context, MyReceiver.class);
+        DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+        dpm.setKeyguardDisabledFeatures(name, DevicePolicyManager.KEYGUARD_DISABLE_FEATURES_NONE);
+        Log.d(TAG, "Unlock successful, enabling fingerprint");
+    }
+}
